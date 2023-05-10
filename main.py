df505faf2b609acde89325493b78149a2e7b8a05
James Aules <james_aules@icloud.com>
2023-05-09 20:20:48 -0400

    treemap

--- main.py
+++ main.py
@@ -24,7 +24,20 @@ Problem 2: Write a function unflatten_dict to do reverse of flatten_dict.
 {'a': 1, 'b': {'x': 2, 'y': 3}, 'c': 4}
 '''
 
+def unflatten_dict(d):
+    res ={}
+    for k, value in d.items():
+        parts = k.split('.')
+        current = res
+        for part in parts[:-1]:
+            if part not in current:
+                current[part] = {}
+            current = current[part]
+        current[parts[-1]] = value
+    return res
 
+
+print(unflatten_dict({'a': 1, 'b.x': 2, 'b.y': 3, 'c': 4}))
 '''
 Problem 3: Write a function treemap to map a function over nested list.
 
@@ -33,3 +46,10 @@ Problem 3: Write a function treemap to map a function over nested list.
 '''
 
 
+def treemap(func, nestedList):
+    if isinstance(nestedList, list):
+        return [treemap(func, x) for x in nestedList]
+    else:
+        return func(nestedList)
+
+print(treemap(lambda x: x*x, [1, 2, [3, 4, [5]]]))
